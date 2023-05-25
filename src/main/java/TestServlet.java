4fe453526f228e1f9bfb36fef085b09b9c50c0b5
peipei30 <133873451+peipei30@users.noreply.github.com>
2023-05-25 14:08:52 +0800

    progect initialize

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hi") // 註冊給web container看 讓container幫我們創建實體
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.print("hi");
+		
+	}
+}
